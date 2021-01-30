7222648980a37b8d7e2da77dd516a027020ae32b
dev-sv <useroleg910@gmail.com>
2021-01-30 20:58:07 +0300

    hdmi edt

--- FpgaProjects/hdmi/DOC/max10_hdmi/altddio_out1_inst.v
+++
@@ -1,6 +0,0 @@
-altddio_out1	altddio_out1_inst (
-	.datain_h ( datain_h_sig ),
-	.datain_l ( datain_l_sig ),
-	.outclock ( outclock_sig ),
-	.dataout ( dataout_sig )
-	);
--- FpgaProjects/hdmi/hdmi.sv
+++ FpgaProjects/hdmi/hdmi.sv
@@ -1,11 +1,18 @@
 
 
+import pkg_disp::t_sync;
+
 
 module hdmi(input bit osc, input bit[7:0] red, green, blue,  output  bit clk_p, clk_n, red_p, red_n, 
 														green_p, green_n, blue_p, blue_n, output bit[10:0] x, y); 
 
+// sync params.
+t_sync sp = '{horz_front_porch:24, horz_sync:136, horz_back_porch:160, horz_pix:1024,
+
+				  vert_front_porch:10, vert_sync:8, vert_back_porch:30, vert_pix:600};
 
 
+													
 wire  w_de,
 		w_clk, 
 		q_clk,  
@@ -27,7 +34,7 @@ wire [9:0]  w_out_r,
 	assign clk_n = ~q_clk;
 
 
-	sync sync_inst(.clk(clk_p), .de(w_de), .vh(w_vh), .x(x), .y(y));
+	sync sync_inst(.clk(clk_p), .de(w_de), .vh(w_vh), .x(x), .y(y), .sp(sp));
 
 	tmds_encoder tmds_encoder_red(.clk(clk_p), .de(w_de), .vh(2'b00), .d(red), .d_out(w_out_r));
 	tmds_serial  tmds_serial_red(.clk_x10(w_clk_x10), .d(w_out_r), .p(red_p), .n(red_n)); 
